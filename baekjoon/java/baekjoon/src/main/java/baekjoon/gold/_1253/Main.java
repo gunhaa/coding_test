71081d90167a3ac261d81d84d612244903c0bace
gunha <wh8299@gmail.com>
2025-05-01 16:26:51 +0900

    feat : 1253 해결

--- baekjoon/java/baekjoon/src/main/java/baekjoon/gold/_1253/Main.java
+++ baekjoon/java/baekjoon/src/main/java/baekjoon/gold/_1253/Main.java
@@ -1,10 +1,17 @@
 package baekjoon.gold._1253;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Main {
 
-    /*
+    /* http://acmicpc.net/problem/1253
     좋다
-    시간 제한	메모리 제
+    시간 제한	메모리 제한
         2 초	256 MB
     문제
     N개의 수 중에서 어떤 수가 다른 수 두 개의 합으로 나타낼 수 있다면 그 수를 “좋다(GOOD)”고 한다.
@@ -20,4 +27,55 @@ public class Main {
     좋은 수의 개수를 첫 번째 줄에 출력한다.
 
     */
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/main/java/baekjoon/gold/_1253/input"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int answer = 0;
+        // 전체 크기 2000
+        // left idx, rigth idx를 내려오는 탐색해야함
+        // '다른' 이라는 케이스를 생각해야한다
+        Arrays.sort(arr);
+        for (int i = 0; i < N; i++) {
+            if (isGoodNumber(arr, i)) {
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    static boolean isGoodNumber(int[] arr , int targetIdx) {
+        int leftIdx = 0;
+        int rightIdx = arr.length-1;
+
+        while (leftIdx < rightIdx) {
+
+            if(leftIdx == targetIdx){
+                leftIdx++;
+                continue;
+            }
+
+            if(rightIdx == targetIdx){
+                rightIdx--;
+                continue;
+            }
+
+            int sum = arr[leftIdx] + arr[rightIdx];
+            if (sum == arr[targetIdx]) {
+                return true;
+            } else if (sum < arr[targetIdx]) {
+                leftIdx = leftIdx + 1;
+            } else if (sum > arr[targetIdx]) {
+                rightIdx = rightIdx - 1;
+            }
+        }
+        return false;
+    }
 }
