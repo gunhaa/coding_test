59ed6370d1c8c5afcbb0bf732747976f19eb2aae
gunha <wh8299@gmail.com>
2025-07-01 16:02:58 +0900

    baekjoon - silver3 - 11727 - dp,점화식 구현, dp가 의심스러울때 직접 그려본 뒤 규칙 찾아보는 방향으로 진행

--- baekjoon/java/baekjoon/src/main/java/baekjoon/silver/_11727/Main.java
+++ baekjoon/java/baekjoon/src/main/java/baekjoon/silver/_11727/Main.java
@@ -1,17 +1,20 @@
-package baekjoon.silver._11726;
+package baekjoon.silver._11727;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     /* s3
-    2×n 타일링
+    2×n 타일링 2
     시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
-         1 초	256 MB	208520	81242	60338	36.925%
+    1 초	256 MB	91474	53917	43699	58.344%
     문제
-    2×n 크기의 직사각형을 1×2, 2×1 타일로 채우는 방법의 수를 구하는 프로그램을 작성하시오.
+    2×n 직사각형을 1×2, 2×1과 2×2 타일로 채우는 방법의 수를 구하는 프로그램을 작성하시오.
 
-    아래 그림은 2×5 크기의 직사각형을 채운 한 가지 방법의 예이다.
+    아래 그림은 2×17 직사각형을 채운 한가지 예이다.
 
     입력
     첫째 줄에 n이 주어진다. (1 ≤ n ≤ 1,000)
@@ -23,17 +26,17 @@ public class Main {
     static final int cont = 10007;
     static int[] memo;
     public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("src/main/java/baekjoon/silver/_11726/input"));
+        System.setIn(new FileInputStream("src/main/java/baekjoon/silver/_11727/input"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int width = Integer.parseInt(br.readLine());
         // 2xn = f(n)의 총 방법수는
-        // f(n) = f(n-2) + f(n-1)
+        // f(n) = f(n-2)*2 + f(n-1)
         memo = new int[width+1];
         memo[0] = 1;
         memo[1] = 1;
 
         for (int i = 2; i <= width; i++) {
-            memo[i] = (memo[i-1] + memo[i-2]) % 10007;
+            memo[i] = (memo[i-1] + memo[i-2]*2) % cont;
         }
 
         System.out.println(Arrays.toString(memo));
