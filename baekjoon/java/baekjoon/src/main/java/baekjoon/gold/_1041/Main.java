d680a80204814aec4281cd2c5091880aba86d340
gunha <wh8299@gmail.com>
2025-07-05 16:44:07 +0900

    baekjoon - gold5 - 1041 - 수정 완료, 각 케이스별 로직 구현하여 수정

--- baekjoon/java/baekjoon/src/main/java/baekjoon/gold/_1041/Main.java
+++ baekjoon/java/baekjoon/src/main/java/baekjoon/gold/_1041/Main.java
@@ -16,6 +16,8 @@ public class Main {
     +---+---+---+---+
         | C |
         +---+
+
+
     주사위는 위와 같이 생겼다. 주사위의 여섯 면에는 수가 쓰여 있다. 위의 전개도를 수가 밖으로 나오게 접는다.
 
     A, B, C, D, E, F에 쓰여 있는 수가 주어진다.
@@ -37,51 +39,72 @@ public class Main {
         System.setIn(new FileInputStream("src/main/java/baekjoon/gold/_1041/input"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int diceN = Integer.parseInt(br.readLine());
-        int[] dice = new int[6];
+        final long diceS = 6;
+        long diceN = Long.parseLong(br.readLine());
+        long[] dice = new long[6];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < dice.length; i++) {
             dice[i] = Integer.parseInt(st.nextToken());
         }
 
-        int answer = 0;
+        long answer = 0;
+
+        if (diceN == 1) {
+            long edgeCaseMax = Long.MIN_VALUE;
+            for (long i : dice) {
+                edgeCaseMax = Math.max(edgeCaseMax, i);
+                answer += i;
+            }
+            answer -= edgeCaseMax;
+            System.out.println(answer);
+            return;
+        }
 
         // NxNxN 에서 밑면 제외시키면
         // 3면 개방 위쪽 구석블록
         // 2면 개방 사이블록
         // 1면 개방 가운데블록 전부
 
-        Arrays.sort(dice);
+
+//        Arrays.sort(dice);
+
         // 최소값 idx - 0 , 1 , 2 순
         // 노출되는 면은 n^2 x 5임
         // 맨위 한개의 면은 특수하며, 4개의 면은 동일해서 x4하면됨
 
         // 블록단위의 loop를 도는게 가장 명확할듯
         // 3면 노출 갯수x 최소~3 - NxN의 위쪽 모서리 4개
-        final int threeSidesBlock = 4;
+        final long threeSidesBlock = 4;
         // 2면이 노출된 블록(N의 개수에 따른 동적 개수/ 갯수x최소~2 / 갯수 (N-2)*4 + (N-1)*4 )
-        final int twoSidesBlock = (diceN-2)*4 + (diceN-1)*4;
+        final long twoSidesBlock = (diceN - 2) * 4 + (diceN - 1) * 4;
+
         // 1면 노출된 블록(N의 갯수에 따른 동적 개수/ 갯수x최소값/ 갯수 (N-2)^2 + (N-2)(N-1)*4
-        final int oneSidesBlock = (diceN-2)*(diceN-2) + (diceN-2)*(diceN-1)*4;
+        final long oneSidesBlock = (diceN - 2) * (diceN - 2) + (diceN - 2) * (diceN - 1) * 4;
 
-        int threeSidesMin = 0;
+        long oneSidesBlockMin = Long.MAX_VALUE;
+
+        for (int i = 0; i < diceS; i++) {
+            if (dice[i] < oneSidesBlockMin) {
+                oneSidesBlockMin = dice[i];
+            }
+        }
+        answer += oneSidesBlock * oneSidesBlockMin;
+
+        long twoSidesBlockMin = Long.MAX_VALUE;
+        for (int i = 0; i < diceS; i++) {
+            for (int j = 0; j < diceS; j++) {
+                if (i + j != 5 && i != j) {
+                    twoSidesBlockMin = Math.min(twoSidesBlockMin, dice[i] + dice[j]);
+                }
+            }
+        }
+        answer += twoSidesBlock * twoSidesBlockMin;
+
+        long threeSidesBlockMin = 0;
         for (int i = 0; i < 3; i++) {
-            threeSidesMin += dice[i];
+            threeSidesBlockMin += Math.min(dice[i], dice[5 - i]);
         }
-        answer += threeSidesMin * threeSidesBlock;
-
-        int twoSidesMin = 0;
-        for (int i=0; i < 2; i++) {
-            twoSidesMin += dice[i];
-        }
-        answer += twoSidesMin * twoSidesBlock;
-
-        int oneSidesMin = 0;
-        for (int i=0; i < 2; i++) {
-            oneSidesMin += dice[i];
-        }
-        answer += oneSidesMin * oneSidesBlock;
-
+        answer += threeSidesBlock * threeSidesBlockMin;
 
         System.out.println(answer);
     }
