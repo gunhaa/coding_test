97503ebec61fe3b0c8a00ac7b4af0a0949a03f6c
gunha <wh8299@gmail.com>
2025-07-18 00:12:17 +0900

    baekjoon - gold5 - 10026 - dfs, 2중 for문을 이용한 구현

--- baekjoon/java/baekjoon/src/main/java/baekjoon/gold/_10026/Main.java
+++ baekjoon/java/baekjoon/src/main/java/baekjoon/gold/_10026/Main.java
@@ -35,4 +35,72 @@ RRRRR
 출력
 적록색약이 아닌 사람이 봤을 때의 구역의 개수와 적록색약인 사람이 봤을 때의 구역의 수를 공백으로 구분해 출력한다.
     */
+
+    static int N;
+    static char[][] grid;
+    static boolean[][] visited;
+    static int normalResult = 0;
+    static int colorResult = 0;
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/main/java/baekjoon/gold/_10026/input"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        grid = new char[N][N];
+        visited = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            char[] line = br.readLine().toCharArray();
+            for (int j = 0; j < N; j++) {
+                grid[i][j] = line[j];
+            }
+        }
+
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j]) {
+                    normalResult++;
+                    dfs(i, j, grid[i][j]);
+                }
+            }
+        }
+
+        visited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if(grid[i][j] == 'G') {
+                    grid[i][j] = 'R';
+                }
+            }
+        }
+
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j]) {
+                    colorResult++;
+                    dfs(i, j, grid[i][j]);
+                }
+            }
+        }
+
+        System.out.println(normalResult + " " + colorResult);
+    }
+
+    static void dfs(int x, int y, char cur) {
+
+        if(x < 0 || x >= N || y < 0 || y >= N || visited[x][y] || cur != grid[x][y]) {
+            return;
+        }
+
+        visited[x][y] = true;
+
+        dfs(x + 1, y, cur);
+        dfs(x - 1, y, cur);
+        dfs(x, y + 1, cur);
+        dfs(x, y - 1, cur);
+    }
+
+
 }
