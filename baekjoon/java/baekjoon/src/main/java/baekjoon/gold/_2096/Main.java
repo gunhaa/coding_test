d523ea22220f54ad709eeeb22d354bf32438b3e1
gunha <wh8299@gmail.com>
2025-07-20 14:25:06 +0900

    baekjoon - gold5 - 2096 - dp, 슬라이딩 윈도우를 이용한 해결

--- baekjoon/java/baekjoon/src/main/java/baekjoon/gold/_2096/Main.java
+++ baekjoon/java/baekjoon/src/main/java/baekjoon/gold/_2096/Main.java
@@ -28,6 +28,63 @@ N줄에 0 이상 9 이하의 숫자가 세 개씩 적혀 있다.
 첫째 줄에 얻을 수 있는 최대 점수와 최소 점수를 띄어서 출력한다.
     */
     public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/main/java/baekjoon/gold/_2096/input"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] searchMax = new int[3];
+        int[] searchMin = new int[3];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int cur0 = Integer.parseInt(st.nextToken());
+            int cur1 = Integer.parseInt(st.nextToken());
+            int cur2 = Integer.parseInt(st.nextToken());
+
+//            System.out.println("cur0 = " + cur0);
+//            System.out.println("cur1 = " + cur1);
+//            System.out.println("cur2 = " + cur2);
+            if (i == 0) {
+                searchMax[0] = cur0;
+                searchMax[1] = cur1;
+                searchMax[2] = cur2;
+
+                searchMin[0] = cur0;
+                searchMin[1] = cur1;
+                searchMin[2] = cur2;
+            } else {
+                int prevMax0 = searchMax[0];
+                int prevMax1 = searchMax[1];
+                int prevMax2 = searchMax[2];
+
+                searchMax[0] = Math.max(prevMax0, prevMax1) + cur0;
+                searchMax[1] = Math.max(prevMax0, Math.max(prevMax1, prevMax2)) + cur1;
+                searchMax[2] = Math.max(prevMax1, prevMax2) + cur2;
+
+                int prevMin0 = searchMin[0];
+                int prevMin1 = searchMin[1];
+                int prevMin2 = searchMin[2];
+
+                searchMin[0] = Math.min(prevMin0, prevMin1) + cur0;
+                searchMin[1] = Math.min(prevMin0, Math.min(prevMin1, prevMin2)) + cur1;
+                searchMin[2] = Math.min(prevMin1, prevMin2) + cur2;
+            }
+//            System.out.println(Arrays.toString(searchMax));
+//            System.out.println(Arrays.toString(searchMin));
+
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int candidate : searchMax) {
+            max = Math.max(candidate, max);
+        }
+
+        int min = Integer.MAX_VALUE;
+        for(int candidate : searchMin) {
+            min = Math.min(candidate, min);
+        }
+        System.out.println(max + " " + min);
+
+//        System.out.println(Arrays.deepToString(grid));
 
     }
 }
